d002c850225f5abf2cd1a7f70e8098bda6cbe3ff
zhujiahui <zhujiahui_zjohn@foxmail.com>
2022-06-06 17:06:26 +0800

    Update main.cpp

--- ImageProc/Interploation/main.cpp
+++ ImageProc/Interploation/main.cpp
@@ -7,7 +7,7 @@ void resizeBicubic(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx = 0, dou
 
 int main()
 {
-  cv::Mat lena = cv::imread(R"(D:\VisualStudio\CVPractice\ImageProc\Lena.png)");
+  cv::Mat lena = cv::imread(R"(Lena.png)");
 
   cv::Mat dst;
   resizeBilinear(lena, dst, cv::Size(1024, 1024));
