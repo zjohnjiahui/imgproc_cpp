5a1bdbbdf87165b30263dd1c01c72f7f9925dfcb
zhujiahui <zhujiahui_zjohn@foxmail.com>
2022-06-06 16:03:13 +0800

    Update resizeBilinear.cpp

--- ImageProc/Interploation/resizeBilinear.cpp
+++ ImageProc/Interploation/resizeBilinear.cpp
@@ -1,3 +1,6 @@
+//refer:
+// https://blog.csdn.net/weixin_40647819/article/details/86601070
+// https://www.cnblogs.com/Imageshop/archive/2011/11/12/2246808.html
 
 void resizeBilinear(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx=0, double sy=0) {
   int dst_rows;
@@ -52,7 +55,7 @@ void resizeBilinear(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx=0, doub
   }
 }
 
-void resizeBilinearTurbo(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx = 0, double sy = 0)
+void resizeBilinearQuantization(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx = 0, double sy = 0)
 {
   int dst_rows;
   int dst_cols;
