09249cff67392d5c4bd7a5498c26a1adbff6a47b
zhujiahui <zhujiahui_zjohn@foxmail.com>
2022-06-16 21:42:56 +0800

    Update resizeNearest.cpp
    add namespace

--- ImageProc/Interploation/resizeNearest.cpp
+++ ImageProc/Interploation/resizeNearest.cpp
@@ -1,46 +1,61 @@
 #include<opencv2/opencv.hpp>
 
-void resizeNearest(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx, double sy) {
-  int dst_rows;
-  int dst_cols;
+namespace Prac {
 
-  if (size.height != 0 && size.width != 0) {
-    dst_rows = size.height;
-    dst_cols = size.width;
-    sy = static_cast<double>(dst_rows) / src.rows;
-    sx = static_cast<double>(dst_cols) / src.cols;
-  }
-  else if (sx != 0 && sy != 0) {
-    dst_rows = static_cast<int>(round(sy*src.rows));
-    dst_cols = static_cast<int>(round(sx*src.cols));
-  }
-  else {
-    dst = src.clone();
-    return;
-  }
+  void resizeNearest(cv::Mat& src, cv::Mat& dst, cv::Size size, double sx=0, double sy=0) {
+    int dst_rows;
+    int dst_cols;
 
-  dst = cv::Mat(dst_rows, dst_cols, src.type());
+    if (size.height != 0 && size.width != 0) {
+      dst_rows = size.height;
+      dst_cols = size.width;
+      sy = static_cast<double>(dst_rows) / src.rows;
+      sx = static_cast<double>(dst_cols) / src.cols;
+    }
+    else if (sx != 0 && sy != 0) {
+      dst_rows = static_cast<int>(round(sy*src.rows));
+      dst_cols = static_cast<int>(round(sx*src.cols));
+    }
+    else {
+      dst = src.clone();
+      return;
+    }
 
-  for (int i = 0; i < dst.rows; i++) {
-    double index_i = (i + 0.5) / sy - 0.5; // index i mapped to src
-    if (index_i < 0) index_i = 0;
-    if (index_i > src.rows - 1) index_i = src.rows - 1;
+    dst = cv::Mat(dst_rows, dst_cols, src.type());
 
-    int i1 = static_cast<int>(round(index_i));
+    for (int i = 0; i < dst.rows; i++) {
+      double index_i = (i + 0.5) / sy - 0.5; // index i mapped to src
+      if (index_i < 0) index_i = 0;
+      if (index_i > src.rows - 1) index_i = src.rows - 1;
 
-    for (int j = 0; j < dst.cols; j++) {
+      int i1 = static_cast<int>(round(index_i));
 
-      double index_j = (j + 0.5) / sx - 0.5; // index j mapped to src
-      if (index_j < 0) index_j = 0;
-      if (index_j > src.cols - 1) index_j = src.cols - 1;
+      for (int j = 0; j < dst.cols; j++) {
 
-      int j1 = static_cast<int>(round(index_j));
+        double index_j = (j + 0.5) / sx - 0.5; // index j mapped to src
+        if (index_j < 0) index_j = 0;
+        if (index_j > src.cols - 1) index_j = src.cols - 1;
 
-      uchar* ptr = dst.ptr<uchar>(i, j);
-      for (int c = 0; c < src.channels(); ++c)
-      {
-        ptr[c] = static_cast<uchar>(src.ptr<uchar>(i1, j1)[c]);
+        int j1 = static_cast<int>(round(index_j));
+
+        uchar* ptr = dst.ptr<uchar>(i, j);
+        for (int c = 0; c < src.channels(); ++c)
+        {
+          ptr[c] = static_cast<uchar>(src.ptr<uchar>(i1, j1)[c]);
+        }
       }
     }
   }
+
+}// namespace
+
+
+void testNearest()
+{
+  cv::Mat lena = cv::imread(R"(D:\VisualStudio\CVPractice\ImageProc\Lena.png)");
+
+  cv::Mat dst;
+  Prac::resizeNearest(lena, dst, cv::Size(1024, 1024));
+
+  return;
 }
