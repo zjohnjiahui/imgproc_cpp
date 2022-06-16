bfc05e36b10cdd38796dba5bd8f9c303dba3df9d
zhujiahui <zhujiahui_zjohn@foxmail.com>
2022-06-16 21:52:36 +0800

    Update gammaCorrection.cpp

--- ImageProc/Grayscale/gammaCorrection.cpp
+++ ImageProc/Grayscale/gammaCorrection.cpp
@@ -7,40 +7,44 @@
 // https://zhuanlan.zhihu.com/p/36581276
 // https://blog.csdn.net/qq_15972907/article/details/123748166
 
-void makeGammaLUT(uchar LUT[], float gamma)
-{
-  for (int i = 0; i < 256; ++i)
-  {
-    float cur_f = i / 255.0f; // normalize to [0, 1]
-    cur_f = powf(cur_f, gamma); // I^r
-    LUT[i] = cv::saturate_cast<uchar>(cur_f * 255.0f;
-  }
-}
+namespace Prac {
 
-void applyLUT(cv::Mat& src, uchar LUT[])
-{  
-  for (int i = 0; i < src.rows; ++i)
+  void makeGammaLUT(uchar LUT[], float gamma)
   {
-    uchar* ptr = src.ptr<uchar>(i, 0);
-    for (int j = 0; j < src.cols; ++j)
+    for (int i = 0; i < 256; ++i)
     {
-      for (int c = 0; c < src.channels(); ++c)
-      {
-        ptr[c] = LUT[ptr[c]];
-      }
-      ptr += src.channels();
+      float cur_f = (i + 0.5f) / 255; // normalize to [0, 1]
+      cur_f = powf(cur_f, gamma); // I^r
+      LUT[i] = cv::saturate_cast<uchar>(cur_f*255.0f - 0.5f);
     }
   }
-}
 
-int main()
+  void applyLUT(cv::Mat& src, uchar LUT[])
+  {
+    for (int i = 0; i < src.rows; ++i)
+    {
+      uchar* ptr = src.ptr<uchar>(i, 0);
+      for (int j = 0; j < src.cols; ++j)
+      {
+        for (int c = 0; c < src.channels(); ++c)
+        {
+          ptr[c] = LUT[ptr[c]];
+        }
+        ptr += src.channels();
+      }
+    }
+  }
+
+} //namespace 
+
+void testGammaCorrection()
 {
   cv::Mat lena = cv::imread(R"(Lena.png)");
 
   uchar LUT[256];
-  makeGammaLUT(LUT, 1/2.2f);
-  applyLUT(lena, LUT);
+  Prac::makeGammaLUT(LUT, 2);
+  Prac::applyLUT(lena, LUT);
   cv::imshow("lena", lena);
 
-  return 0;
+  return;
 }
